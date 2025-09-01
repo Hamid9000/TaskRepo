64879c4cccf6d3758da453f5d6a7f91f16b4386f
Sheikh Hamid <147171592+Hamid9000@users.noreply.github.com>
2025-09-01 19:00:58 +0530

    Initial commit

---
+++ src/LoginPageTest.java
@@ -0,0 +1,12 @@
+public class LoginPageTest {
+    public static void main(String[]args){
+        // Zoya - Login Page UI Test
+        // 1. Check username field
+        // 2. Check password field
+        // 3. Check login button
+        System.out.println("Zoya - Login Page UI Test");
+        System.out.println("1. Check username field");
+        System.out.println("2. Check password field");
+        System.out.println("3. Check login button");
+    }
+}
